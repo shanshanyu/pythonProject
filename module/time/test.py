1bacb0526f74135f3f687b0665a0a48f8976a8e4
yushanshan <yushanshan@MacBook-Pro-2.local>
2023-05-11 18:46:15 +0800

    new module

--- module/time/test.py
+++ module/time/test.py
@@ -3,3 +3,18 @@ create_time: 2023/5/11 15:34
 author: yss
 version: 1.0
 '''
+
+import time
+
+print(time.gmtime(0))
+
+print(time.gmtime()) #当前的 UTC时间
+
+print(time.localtime()) #当前的时间
+
+
+print(time.strftime("%Y-%m-%d-%H:%M:%S",time.localtime()))
+
+import os.path
+
+print(os.path.abspath(__file__))
