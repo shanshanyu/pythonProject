24fbe7a647b2fbb03ffada5fe69bd4297f7549f9
shanshanyu <1525996190@qq.com>
2023-05-28 20:47:15 +0800

    new pc test

--- file_io/read_chinese.py
+++ file_io/read_chinese.py
@@ -10,7 +10,6 @@ content = f1.read()
 print(content)
 f1.close()
 
-f2 = open('test.txt','rb')
+f2 = open('test.txt','r')    #二进制文件采用 b 模式，readline  readlines  writeline  writelines 一般适用文本文件
 data = f2.readline()
 print(data,type(data))
-print(data.decode())
