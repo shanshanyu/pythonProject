b116a5292bca560f11d2fc14192e4bafcf58ea6f
shanshanyu <1525996190@qq.com>
2023-05-31 15:33:16 +0800

    new file

--- list/create_list.py
+++ list/create_list.py
@@ -11,7 +11,7 @@ print(a,type(a))
 
 
 b = list((1,2,3))
-b = list({'a':1,'b':2})  #给 list()函数传递字典，会把列表的 key 放在列表中
+b = list({'a':1,'b':2})  #给 list()函数传递字典，会把字典的 key 放在列表中
 print(b,type(b))
 
 c = [i for i in range(5)]
@@ -22,3 +22,10 @@ print(c,type(c))
 d = '[1,2,3]'
 d1 = list(d)  #不符合预期，会把 d 当做一个列表元素，字符串
 print(d1)
+
+d = [1,2,3]
+d = d*3  #列表支持加法和乘法
+print(d,type(d))
+
+#list()函数 可以将 元组  字符串   range对象 转换为列表
+#tuple()函数可以将列表、range对象转换为元组
