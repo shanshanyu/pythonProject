49f2a0aeb2dc49af77769b61c3d3facebdcf3a2f
shanshanyu <1525996190@qq.com>
2023-06-30 22:07:06 +0800

    new file

--- kafka_test/subscribe_msg_bytime_v1.py
+++ kafka_test/subscribe_msg_bytime_v1.py
@@ -1,7 +1,14 @@
 '''
 create_time: 2023/6/28 18:21
 author: yss
-version: 1.0
+version: 1.1
+做了一些修改：
+1. 读取正在写入的时间点时：读到当前offset最大值+1 退出，避免程序一直执行
+2. 根据指定时间点找不到 offset 时，打印异常信息
+
+剩余问题：
+任何一个partition读取到指定的时间戳后停止拉取，可能会存在数据未读完的情况
+读取到所有partition中最大的offset时停止拉取，可能会存在数据未读完的情况
 '''
 
 #订阅某个 topic  某个时间点的数据，时间点是小时
@@ -15,9 +22,11 @@ def get_offset_by_time(topic_name,bootstrap_servers,timestamp):  #根据timestam
     partitions = consumer.partitions_for_topic(topic_name)  #获取 topic 有哪些 partitions
 
     offsets = {}
+
     for partition in partitions:
         tp = TopicPartition(topic_name,partition)  #TopicPartition 对象
         offsets[tp] = consumer.offsets_for_times({tp:timestamp})  #值是一个字典，键是TopicPartition对象，值是OffsetAndTimestamp对象
+
 
     consumer.close()
     return offsets  #offsets 是一个字典
@@ -32,7 +41,10 @@ def subscribe_topic(bootstrap_servers,start_offset,end_time):
 
 
     for item in start_offset.values(): #设置seek 的 offset
-        for key, value in item.items():
+        for key, value in item.items():  #如果指定时间点的offset 不存在，offsets_for_times的返回值{partition:None}
+            if not value:
+                print('指定的时间点找不到数据，检查输入的时间是否正确以及服务器的时区')
+                exit(1)
             consumer.seek(key,value.offset)
 
 
@@ -40,8 +52,13 @@ def subscribe_topic(bootstrap_servers,start_offset,end_time):
 
     messages = []  #消息列表
 
+    max_offset = consumer.end_offsets(consumer.assignment()).values()
+    max_offset = max(max_offset)  # 获取最大偏移量
+
+    #print(consumer.end_offsets(consumer.assignment())) 是一个字典，key是partition对象，值是 end_offset
+
     for msg in consumer:
-        if msg.timestamp >= end_timestamp:
+        if msg.timestamp >= end_timestamp or msg.offset >= max_offset-1:
             break
         messages.append(msg.value.decode())
 
---
+++ python_basic/dict/test_seq_encap.py
@@ -0,0 +1,9 @@
+'''
+create_time: 2023/6/30 21:58
+author: yss
+version: 1.0
+'''
+a = {1:2}
+
+c,d = a  #报错，想实现的效果是把字典元素的 key value 分别给两个变量
+print(c,d)
