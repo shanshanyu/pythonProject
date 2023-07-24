a283f09ffa3891e823a048b4e713b4c4080875be
shanshanyu <1525996190@qq.com>
2023-07-24 18:58:02 +0800

    new file

--- module/os_path/path.py
+++ module/os_path/path.py
@@ -2,5 +2,10 @@
 create_time: 2023/5/10 16:19
 author: yss
 version: 1.0
+os.path.dirname(path)是用split()切分path，然后返回第一部分，split()会把一个path切分成两部分，head和tail，如果路径中没有/，head为空，如果结尾是/，tail为空。
 '''
 
+import os.path
+
+print(os.path.abspath('path.py'))
+print(os.path.dirname('path.py'))  #返回空
--- trust_new/trust1.py
+++ trust_new/trust1.py
@@ -4,8 +4,10 @@ author: yss
 version: 1.0
 ssh免密脚本
 '''
-
+import os.path
+import sys
 import paramiko
+import yaml
 
 
 class SSHClient:
@@ -50,8 +52,54 @@ class SSHClient:
             print('命令执行失败')
 
 
+class Trust:
+    def __init__(self,conf_file):
+        self.conf = self.parse_conf(conf_file)
+
+    def parse_conf(self,conf_file):
+        #print(os.path.abspath(conf_file))
+        if not os.path.exists(conf_file):   #判断文件是否存在
+            raise Exception('{} 文件不存在'.format(conf_file))
+
+        with open(conf_file,'r') as f:  #解析文件
+            try:
+                conf = yaml.full_load(f.read())
+            except Exception as e:
+                print(e)
+
+        required_keys = ['ips','ssh_port','password','user','trust_user']
+        list_type_keys = ['ips','password']
+
+        for i in required_keys:
+            if i not in conf.keys():
+                print('配置文件错误,{}不存在'.format(i))
+                sys.exit(1)
+
+        for i in list_type_keys:
+            if not isinstance(conf[i],list):
+                print('{} 需要是列表类型'.format(i))
+                sys.exit(1)
+
+        if len(conf['ips']) != len(conf['password']):
+            print('ip 和 密码长度不相等')
+            sys.exit(1)
+
+        conf['ssh_params'] = {}
+        for i in conf['ips']:
+            conf['ssh_params'][i] = {'hostname':i,'port':conf['ssh_port'],'username':conf['user'],'password':conf['password'][conf['ips'].index(i)]}
+
+        return conf
+
+
+if __name__ == '__main__':
+    t = Trust('../trust/test1.yml')
+    print(t.conf)
+
+'''
 if __name__ == '__main__':
     client = SSHClient(hostname='10.129.20.81',username='root',password='MhxzKhl2015')
-    ret = client.run_cmd('ls /')
-    print(ret)
+    result = client.run_cmd('lbn /')
+    print(result)
+'''
+
 
