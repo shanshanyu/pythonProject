d09e8d468cad261c8e3776fc979ccfdf69ec1b8b
yushanshan <yushanshan@sensorsdata.cn>
2024-04-09 16:54:35 +0800

    new file

--- optoolkit/tools/menu.py
+++ optoolkit/tools/menu.py
@@ -1,14 +1,8 @@
-'''
-create_time: 2023/12/29 10:27
-author: yss
-version: 1.0
-'''
-
-import signal
+import re
 import sys
 import termios
 import tty
-import re
+import signal
 
 
 class Menu(object):
@@ -16,6 +10,7 @@ class Menu(object):
     命令行菜单
     """
     def __init__(self):
+
         self._term_start_pos = 0
         self._term_end_pos = 0
         self.offset = " " * 8  # 菜单距离左侧偏移量
@@ -25,7 +20,7 @@ class Menu(object):
         self.page_size = 10  # 每页显示多少条
         self.title_delimiter = " > "  # 定义标题分隔符
         self.pointer = "➔ "  # 定义选择指示器
-        self.background = ""  # 背景颜色
+        self.background = ""   # 背景颜色
         self.title_color = "purple"
         self.help_color = "blue"
         self.foot_color = "yellow"
@@ -33,32 +28,35 @@ class Menu(object):
         self.body_on_switch_color = "green"
 
     def style(
-            self,
-            id_show=True,
-            title_show=True,
-            foot_show=True,
-            page_size=10,
-            pointer="➔ ",
-            title_color="purple",
-            help_color="blue",
-            foot_color="yellow",
-            body_color="white",
-            body_on_switch_color="green",
+        self,
+        id_show=True,
+        title_show=True,
+        foot_show=True,
+        page_size=10,
+        pointer="➔ ",
+        title_color="purple",
+        help_color="blue",
+        foot_color="yellow",
+        body_color="white",
+        body_on_switch_color="green",
     ):
         """
         菜单样式设置
         供选择的颜色：'black', 'red', 'green', 'yellow',
                     'blue', 'purple', 'cyan', 'white'
-        :param id_show: 是否显示行数
-        :param title_show: 是否显示标题
-        :param foot_show:  是否显示页脚
-        :param page_size:  每页显示多少条
-        :param pointer:  设置指示器，默认(➔)
-        :param title_color: black','red','green','yellow','blue','purple','cyan','white'
-        :param help_color: black','red','green','yellow','blue','purple','cyan','white'
-        :param foot_color: black','red','green','yellow','blue','purple','cyan','white'
-        :param body_color: black','red','green','yellow','blue','purple','cyan','white'
-        :param body_on_switch_color: black','red','green','yellow','blue','purple','cyan','white'
+        :param id_show:   是否显示行数
+        :param title_show:  # 是否显示标题
+        :param foot_show:   # 是否显示页脚
+        :param page_size:   # 每页显示多少条
+        :param pointer:     # 设置指示器，默认(➔)
+        :param title_color:
+            'black','red','green','yellow','blue','purple','cyan','white'
+        :param foot_color:
+            'black', 'red','green','yellow','blue','purple','cyan','white'
+        :param body_color:
+            'black', 'red', 'green','yellow','blue','purple','cyan','white'
+        :param body_on_switch_color:
+        'black', 'red', 'green', 'yellow', 'blue', 'purple', 'cyan', 'white'
         :return:
         """
         self.id_show = id_show  # 是否显示列表id
@@ -72,12 +70,38 @@ class Menu(object):
         self.body_color = body_color  # 主体文字颜色
         self.body_on_switch_color = body_on_switch_color  # 选择的主体文字颜色
 
+    def color_style(self, string, mode="", font_color="", background=""):
+        """
+        字体样式选择
+        """
+        styles = {
+            "font_color": {"black": 30, "red": 31, "green": 32, "yellow": 33,
+                           "blue": 34, "purple": 35, "cyan": 36, "white": 37,
+                           },
+            "background": {"black": 40, "red": 41, "green": 42, "yellow": 43,
+                           "blue": 44, "purple": 45, "cyan": 46, "white": 47,
+                           },
+            "mode": {"bold": 1, "underline": 4, "blink": 5, "invert": 7},
+            "default": {"end": 0},
+        }
+        mode = str(styles["mode"].get(mode, ""))
+        fore = str(styles["font_color"].get(font_color, ""))
+        back = styles["background"].get(background, "")
+        _end = styles["default"].get("end", 0)
+        # \033[参数1;参数2;参数3m   参数1样式，参数2字体颜色，参数3 背景色
+        _style = ";".join([str(s) for s in [mode, fore, back] if s])
+        style = f"\033[{_style}m"
+        end = f"\033[{_end}m"
+        return f"{style}{string}{end}"
+
     def get_pos(self):
         """
         获取当前光标位置
         """
         buf = ""
+        #获取标准输入文件描述符
         stdin = sys.stdin.fileno()
+        #
         tattr = termios.tcgetattr(stdin)
 
         try:
@@ -86,6 +110,7 @@ class Menu(object):
             sys.stdout.flush()
 
             while True:
+                #buf 收到的返回值类型于  b'\x1b[59;1R'
                 buf += sys.stdin.read(1)
                 if buf[-1] == "R":
                     break
@@ -111,138 +136,67 @@ class Menu(object):
             sys.stdout.write("\r\033[K\033[1A")
             sys.stdout.flush()
 
-    def menu_box(self, choose, pos, page_data, page, start,
-                 title=None, guide=None):
+    def get_ch(self):
         """
-        展示整个选择菜单，原理清空屏幕后重新打印数据
+        获取键盘输入
         """
-        self.clear_screen()
-        total = len(choose)
-        self.title_box(title)
-        if guide:
-            self.help_box(guide)
-        self.body_box(pos, page_data)
-        self.foot_box(total, start, page)
+        _input = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(_input)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            choice = sys.stdin.read(1)
+            if choice == "\x1b":
+                choice += sys.stdin.read(2)
+        finally:
+            termios.tcsetattr(_input, termios.TCSADRAIN, old_settings)
+        return choice
 
-    def draw(self,data, title=None, guide=None):
+    def title_box(self, title):
         """
-            绘制表格
+        title block
         """
-        pos, start, page = 0, 0, 1
-        page_size = self.page_size
+        if self.title_show:
+            title_base = "\n" + self.offset + \
+                (len(self.pointer) + 1) * " " + "Main Menu"
+            if title and isinstance(title, list):
+                title_info = (
+                    title_base + self.title_delimiter +
+                    self.title_delimiter.join(title)
+                )
+            else:
+                title_info = title_base
 
-        if title and isinstance(title, list) is False :
-            title = [str(title)]
-        if guide and isinstance(guide, list) is False :
-            guide = [str(guide)]
-        data = [[i, v] for i, v in enumerate(data)]
-        choose_list = data
+            result = self.color_style(
+                title_info,
+                mode="bold",
+                background=self.background,
+                font_color=self.title_color
+            ) + "\n\n"
+            sys.stdout.write(result)
+            sys.stdout.flush()
 
-        signal.signal(signal.SIGINT, self.menu_box)
-
-        while True:
-            total = len(choose_list)
-            if start + page_size < total :
-                page_list = choose_list[start : start + page_size]
-            else :
-                page_list = choose_list[start :total]
-
-            # 控制指针到达边界时
-            if pos < 0:
-                pos = len(page_list) - 1
-            elif pos >= len(page_list) :
-                pos = 0
-
-            self.menu_box(choose_list, pos, page_list,
-                          page, start, title, guide)
-
-            key = self.get_ch()
-            if key == "\r":  # enter
-                if len(choose_list) != 0 :
-                    ids = page_size * (page - 1) + pos
-                    return choose_list[ids][0], choose_list[ids][1]
-
-            elif key in ["b", "B", "q", "Q"] :  # 返回
-                return -1, None
-
-            elif key in ["\x1b[A", "k", "K"] :
-                pos -= 1
-
-            elif key in ["\x1b[B", "j", "J"] :
-                pos += 1
-
-            elif key in ["\x1b[D", "h", "H"] :  # 上一页
-                if start - page_size >= 0 :
-                    pos = 0
-                    start = start - page_size
-                    page -= 1
-
-            elif key in ["\x1b[C", "l", "L"] :  # 下一页
-                if start + page_size < total :
-                    pos = 0
-                    start = start + page_size
-                    page += 1
-
-            elif key in ["S", "s"] :  # 搜索
-                choose_list = self.search_box(choose_list, title)
-
-            elif key in ["X", "x"] :  # 取消搜索
-                choose_list = data
-
-'''
-create_time: 2023/12/29 18:37
-author: yss
-version: 1.0
-'''
-
-import sys
-
-
-class Menu(object):
-    """
-    命令行菜单
-    """
-    def __init__(self):
-
-        self._term_start_pos = 0
-        self._term_end_pos = 0
-        self.offset = " " * 8  # 菜单距离左侧偏移量
-        self.id_show = True  # 是否显示列表id
-        self.title_show = True  # 标题是否显示
-        self.foot_show = True  # 页脚是否显示
-        self.page_size = 10  # 每页显示多少条
-        self.title_delimiter = " > "  # 定义标题分隔符
-        self.pointer = "➔ "  # 定义选择指示器
-        self.background = ""   # 背景颜色
-        self.title_color = "purple"
-        self.help_color = "blue"
-        self.foot_color = "yellow"
-        self.body_color = "white"
-        self.body_on_switch_color = "green"
-
-    def color_style(self,string, mode="", font_color="", background="") :
+    def help_box(self, content):
         """
-        字体样式选择
+        title block
         """
-        styles = {
-            "font_color" : {"black" : 30, "red" : 31, "green" : 32, "yellow" : 33,
-                            "blue" : 34, "purple" : 35, "cyan" : 36, "white" : 37,
-                            },
-            "background" : {"black" : 40, "red" : 41, "green" : 42, "yellow" : 43,
-                            "blue" : 44, "purple" : 45, "cyan" : 46, "white" : 47,
-                            },
-            "mode" : {"bold" : 1, "underline" : 4, "blink" : 5, "invert" : 7},
-            "default" : {"end" : 0},
-        }
-        mode = str(styles["mode"].get(mode, ""))
-        fore = str(styles["font_color"].get(font_color, ""))
-        back = styles["background"].get(background, "")
-        _end = styles["default"].get("end", 0)
-        # 模式，字体，背景色
-        _style = ";".join([s for s in [mode, fore, back] if s])
-        style = f"\033[{_style}m"
-        end = f"\033[{_end}m"
-        return f"{style}{string}{end}"
+        if self.title_show:
+            title_base = self.offset + \
+                (len(self.pointer) + 1) * " " + "Usage"
+            if content and isinstance(content, list):
+                title_info = (
+                    title_base + self.title_delimiter +
+                    self.title_delimiter.join(content)
+                )
+            else:
+                title_info = title_base
+
+            result = self.color_style(
+                title_info,
+                background=self.background,
+                font_color=self.help_color
+            ) + "\n\n"
+            sys.stdout.write(result)
+            sys.stdout.flush()
 
     def body_box(self, pos, choose):
         """
@@ -251,7 +205,7 @@ class Menu(object):
         i = 0
         result = ""
         while i < self.page_size:
-            if i >= len(choose) :
+            if i >= len(choose):
                 result += "\r\n"
                 i += 1
                 continue
@@ -286,25 +240,132 @@ class Menu(object):
         sys.stdout.write(result)
         sys.stdout.flush()
 
-    def draw(self,data,title=None,guide=None):
-        start,pos,page = 0,0,1
+    def foot_box(self, total, start, page):
+        """
+        foot block
+        """
+        result = ""
+        next_page = False
+        prev_page = False
+        if self.foot_show:
+            if start + self.page_size < total:
+                next_page = True
+            if start - self.page_size >= 0:
+                prev_page = True
+
+            foot = (
+                "\n"
+                + self.offset
+                + (len(self.pointer) + 1) * " "
+                + f"< {page} page / {total} tolal"
+            )
+            if next_page and prev_page:
+                foot = foot + " Previous Next"
+            elif next_page:
+                foot = foot + " Next"
+            elif prev_page:
+                foot = foot + " Previous"
+            foot += ">"
+            result = result +\
+                self.color_style(foot,
+                                 background=self.background,
+                                 font_color=self.foot_color) + "\n"
+
+        sys.stdout.write(result)
+        sys.stdout.flush()
+
+    def search_box(self, choose, title):
+        """
+        展示搜索框，原理清空屏幕后重新打印数据
+        """
+        self.clear_screen()
+        search_title = title + ["Search"] if title else ["Search"]
+        self.title_box(search_title)
+        result = "\r\n" + self.offset + "key: "
+        sys.stdout.write(result)
+        sys.stdout.flush()
+        keyword = sys.stdin.readline().strip()
+        filter_choose = list(filter(lambda x: keyword in str(x[1]), choose))
+        return filter_choose
+
+    def menu_box(self, choose, pos, page_data, page, start,
+                 title=None, guide=None):
+        """
+        展示整个选择菜单，原理清空屏幕后重新打印数据
+        """
+        self.clear_screen()
+        total = len(choose)
+        self.title_box(title)
+        if guide:
+            self.help_box(guide)
+        self.body_box(pos, page_data)
+        self.foot_box(total, start, page)
+
+    def draw(self, data, title=None, guide=None):
+        """
+        绘制表格
+        """
+        pos, start, page = 0, 0, 1
         page_size = self.page_size
+
+        if title and isinstance(title, list) is False:
+            title = [str(title)]
+        if guide and isinstance(guide, list) is False:
+            guide = [str(guide)]
         data = [[i, v] for i, v in enumerate(data)]
         choose_list = data
 
-        total = len(choose_list)
-        if start + page_size < total:
-            page_list = choose_list[start:start + page_size]
-        else:
-            page_list = choose_list[start:total]
+        signal.signal(signal.SIGINT, self.menu_box)
 
-        print(page_list)
+        while True:
+            total = len(choose_list)
+            if start + page_size < total:
+                page_list = choose_list[start: start + page_size]
+            else:
+                page_list = choose_list[start:total]
 
-        self.body_box(0, page_list)
+            # 控制指针到达边界时
+            if pos < 0:
+                pos = len(page_list) - 1
+            elif pos >= len(page_list):
+                pos = 0
 
+            self.menu_box(choose_list, pos, page_list,
+                          page, start, title, guide)
+
+            key = self.get_ch()
+            if key == "\r":  # enter
+                if len(choose_list) != 0:
+                    ids = page_size * (page - 1) + pos
+                    return choose_list[ids][0], choose_list[ids][1]
+
+            elif key in ["b", "B", "q", "Q"]:  # 返回
+                return -1, None
+
+            elif key in ["\x1b[A", "k", "K"]:
+                pos -= 1
+
+            elif key in ["\x1b[B", "j", "J"]:
+                pos += 1
+            elif key in ["\x1b[D", "h", "H"]:  # 上一页
+                if start - page_size >= 0:
+                    pos = 0
+                    start = start - page_size
+                    page -= 1
+
+            elif key in ["\x1b[C", "l", "L"]:  # 下一页
+                if start + page_size < total:
+                    pos = 0
+                    start = start + page_size
+                    page += 1
+
+            elif key in ["S", "s"]:  # 搜索
+                choose_list = self.search_box(choose_list, title)
+
+            elif key in ["X", "x"]:  # 取消搜索
+                choose_list = data
 
 if __name__ == '__main__':
-    m = Menu()
     steps = [
         "Allinone Check",
         "Calculate Kudu Tserver Metadata Size",
@@ -319,6 +380,7 @@ if __name__ == '__main__':
         "test1",
         "test2"
     ]
-    m.draw(steps, title="optoolkit",
+    m = Menu()
+    pos = m.draw(steps, title="optoolkit",
                  guide="【Select】↑ ↓ 【choose】Enter 【Search】s/S 【Quit】q/Q/b/B\
-【Page】g/l")
+    【Page】g/l")
