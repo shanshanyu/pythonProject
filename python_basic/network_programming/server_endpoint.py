e28cc9ec7dac976f90fd992913baf5d80b396f2c
ssyu <1525996190@qq.com>
2024-04-28 22:14:03 +0800

    socket test

--- python_basic/network_programming/server_endpoint.py
+++ python_basic/network_programming/server_endpoint.py
@@ -11,13 +11,14 @@ def main():
     server_socket.bind(('127.0.0.1',8888))  # 绑定地址
     server_socket.listen(5)
     print('server start success,waiting client connect ...')
-    while True:
-        #  每当接收到客户端的 socket 请求
-        client_socket,client_addr = server_socket.accept()
-        data = server_socket.recv(1024)
-        print('客户端发送的数据是: ',data.decode('utf-8'))
-        client_socket.send('收到了'.encode('utf-8'))
-        break
+    #  每当接收到客户端的 socket 请求
+    client_socket, client_addr = server_socket.accept()
+    print('收到客户端的请求: ', client_addr)
+    data = client_socket.recv(1024)
+    print('客户端发送的数据是: ', data.decode('utf-8'))
+    client_socket.send('服务器给你一个大笔都'.encode('utf-8'))
+    server_socket.close()
+
 
 
 if __name__ == '__main__':
