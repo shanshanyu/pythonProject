844781229efa95283f1aa931df11a1449b4df052
ssyu <1525996190@qq.com>
2024-07-09 22:14:12 +0800

    new file

---
+++ python_basic/concurrent_programming/multi_process/test_max_progress.py
@@ -0,0 +1,23 @@
+'''
+Data: 2024/7/9 21:58
+Author: yss
+Desc: 测试用户支持多少个进程,应该受 pid_max  nproc的限制
+'''
+import multiprocessing
+import time
+
+
+def test():
+    time.sleep(300)
+
+def main():
+    count = 0;
+    for i in range(1000000):
+        p = multiprocessing.Process(target=test)
+        p.start()
+        count += 1
+        print(count)
+
+
+if __name__ == '__main__':
+    main()
