95490a7a4a1051f88d6f00229b3f9a4f4d94edb8
ssyu <1525996190@qq.com>
2024-12-26 21:24:56 +0800

    html

--- python-web-project/user_login/app.py
+++ python-web-project/user_login/app.py
@@ -2,25 +2,39 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route('/signup',methods=['GET'])
-def signup():
-    return render_template('user_signup.html')
+@app.route('/',methods=['GET'])
+def index():
+    return render_template('index.html')
 
-@app.route('/user_login')   #用户登录
-def user_login():
-    return render_template('user_login.html')
 
-@app.route('/do/reg',methods=['GET'])
-def do_reg():
-    #在这里接受到客户端GET传过来的数据，处理完成返回
-    print(request.args)
-    return '注册成功'
+@app.route('/reg',methods=['GET','POST'])
+def reg():
+    if request.method == 'GET':
+        #在这里接受到客户端GET传过来的数据，处理完成返回
+        return render_template('user_signup.html')
+    elif request.method == 'POST':
+        # 在这里接受到客户端POST传过来的数据，处理完成返回
+        print(request.form)
+        print(request.form.get('username'))
+        print(request.form.getlist('ability'))
+        return '注册成功'
 
-@app.route('/post/reg',methods=['POST'])
-def post_reg():
-    # 在这里接受到客户端POST传过来的数据，处理完成返回
-    print(request.form)
-    return '注册成功'
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('user_login.html')
+    elif request.method == 'POST':
+        user = request.form.get('user')
+        password = request.form.get('pass')
+        print(user,password)
+        if request.form.get('user') == 'abc' and request.form.get('pass') == '123':
+            print('login success')
+            return 'true'
+        else:
+            print('login failed')
+            return 'false'
+
+
 
 if __name__ == '__main__':
     app.run()
